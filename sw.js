0dbb5aee28c2c34b4a14c347a34dffff8032781f
Amal Games <amal@local>
2026-09-08 20:06:45 +0500

    Bust cache for QR sheet (4 admin + 16 friends)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 (function () {
   "use strict";
 
-  var VERSION = "amal-offline-v125";
+  var VERSION = "amal-offline-v126";
   var CORE = VERSION + "-core";
   var RUNTIME = VERSION + "-runtime";
 
