29b346f4987eb65e83a5fdbd8cd246ffd5cfac76
Amal Games <amal@local>
2026-09-09 18:29:47 +0500

    Add version 1.0 archive hub and version travel controls

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 (function () {
   "use strict";
 
-  var VERSION = "amal-offline-v135";
+  var VERSION = "amal-offline-v136";
   var CORE = VERSION + "-core";
   var RUNTIME = VERSION + "-runtime";
 
